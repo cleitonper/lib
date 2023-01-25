f97892df25fae38abf889bd4097dc13e52961e36
Cleiton Pereira <cleiton@weef.com.br>
2023-01-25 11:27:55 -0300

    feat(build): configura rollup como ferramenta de build da biblioteca

---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+const commonjs = require("@rollup/plugin-commonjs").default;
+const resolve = require("@rollup/plugin-node-resolve").default;
+const typescript = require("@rollup/plugin-typescript").default;
+
+const packageJson = require("./package.json");
+
+module.exports = [
+  {
+    input: "src/index.ts",
+    cache: false,
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({
+        tsconfig: "./tsconfig-package.json",
+        outputToFilesystem: true,
+      }),
+    ],
+    external: [
+      "react",
+      "react-dom",
+      'react/jsx-runtime',
+    ],
+  },
+];
