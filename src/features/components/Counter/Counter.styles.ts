d5167d6b5d1fad80ff8a99e7095d8cc56b62b186
Cleiton Pereira <cleiton@weef.com.br>
2023-01-25 19:31:40 -0300

    fix(counter): muda import da função styled

--- src/features/components/Counter/Counter.styles.ts
+++ src/features/components/Counter/Counter.styles.ts
@@ -1,4 +1,4 @@
-import { styled } from '@mui/material/styles';
+import { styled } from '@mui/material';
 import { Stack, Typography } from '@mui/material';
 import { Button } from '~/common/components'
 
